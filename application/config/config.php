140b22ea130c67b7926d795d62c21a8d31b5368f
Uchendu Precious <uchendubozz@gmail.com>
2018-11-22 16:23:42 +0100

    update

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://shufflequotes.herokuapp.com/shufflequotes/';
+$config['base_url'] = 'https://shufflequotes.herokuapp.com/';
 
 /*
 |--------------------------------------------------------------------------
